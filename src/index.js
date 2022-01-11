91b59cd4dca30e32d1a92e551a14548cfc20d9ca
jihaneh <jijih@hotmail.fr>
2022-01-11 15:34:07 +0100

    Remove all hardcoded items from the tasks array

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import DisplayManager from './modules/display_manager.js';
 
 const selector = (element) => document.querySelector(element);
 
-let tasks = [new Task(1, 'one', 'ok'), new Task(2, 'two', 'ok'), new Task(4, 'four', 'ok'), new Task(3, 'three', 'ok'), new Task(5, 'five', 'ok')];
+let tasks = [];
 
 tasks = tasks.sort((a, b) => a.index - b.index);
 
